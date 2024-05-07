20d96979460b948f62f2912d6c21900b38cb5871
Sarlota-Duskova <duskova.s@gmail.com>
2024-05-07 18:48:37 +0200

    Updates

---
+++ precache-manifest.67583d8431ec1e9fa981f97aaebf9e72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c7a0b64fb9e0d3b208dc7edfffda62a",
+    "url": "/Sarlota-Duskova/react-learning/index.html"
+  },
+  {
+    "revision": "052a4b772246ae164e7f",
+    "url": "/Sarlota-Duskova/react-learning/static/css/main.9f108cbc.chunk.css"
+  },
+  {
+    "revision": "9ff56eca5db01625fb33",
+    "url": "/Sarlota-Duskova/react-learning/static/js/2.d8edd5b6.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/Sarlota-Duskova/react-learning/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "052a4b772246ae164e7f",
+    "url": "/Sarlota-Duskova/react-learning/static/js/main.d5ed02eb.chunk.js"
+  },
+  {
+    "revision": "e2352e4334d112238a96",
+    "url": "/Sarlota-Duskova/react-learning/static/js/runtime~main.05599c84.js"
+  }
+]);
